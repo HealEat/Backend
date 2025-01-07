d934cb1e841ed63818d4071b67eeb6372241ddb9
Godot-B <bae0821@gmail.com>
2025-01-07 22:59:27 +0900

    초기 설정 완료

---
+++ src/main/java/healeat/server/domain/enums/NutrientNeeded.java
@@ -0,0 +1,12 @@
+package healeat.server.domain.enums;
+
+// 필요 영양소 정보
+public enum NutrientNeeded {
+
+    CARBOHYDRATES,
+    PROTEINS,
+    FATS,
+    VITAMINS,
+    MINERALS,
+    FIBER
+}
---
+++ src/test/java/healeat/server/HealEatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package healeat.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealEatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
