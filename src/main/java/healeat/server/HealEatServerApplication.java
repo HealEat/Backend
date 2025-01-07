5f91e04d750a91c7b28d92917683554a32015bb9
Godot-B <bae0821@gmail.com>
2025-01-07 22:59:27 +0900

    초기 설정 완료

---
+++ src/main/java/healeat/server/HealEatServerApplication.java
@@ -0,0 +1,12 @@
+package healeat.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealEatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealEatServerApplication.class, args);
+	}
+}
---
+++ src/main/java/healeat/server/domain/enums/Vegeterian.java
@@ -0,0 +1,13 @@
+package healeat.server.domain.enums;
+
+// 베지테리언 종류
+public enum Vegeterian {
+
+    NONE,
+    VEGAN,
+    LACTO,
+    OVO,
+    LACTO_OVO,
+    PESCET,
+    FLEXI
+}
