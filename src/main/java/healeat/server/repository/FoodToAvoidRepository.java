2182f43f8875c5f61d0b3c24467a6b1c1d3dce62
minki <159312137+iKniMiohC@users.noreply.github.com>
2025-01-13 00:53:17 +0900

    Delete src/main/java/healeat/server/repository/FoodToAvoidRepository.java

--- src/main/java/healeat/server/repository/FoodToAvoidRepository.java
+++
@@ -1,7 +0,0 @@
-package healeat.server.repository;
-
-import healeat.server.domain.FoodToAvoid;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodToAvoidRepository extends JpaRepository<FoodToAvoid, Long> {
-}
