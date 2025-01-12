10abbd65fa47d09b23d34610aab75ee79f8229fd
minki <159312137+iKniMiohC@users.noreply.github.com>
2025-01-13 00:58:59 +0900

    [REMOVE] 파일 (NutrientNeededRepository)

--- src/main/java/healeat/server/repository/NutrientNeededRepository.java
+++
@@ -1,7 +0,0 @@
-package healeat.server.repository;
-
-import healeat.server.domain.NutrientNeeded;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NutrientNeededRepository extends JpaRepository<NutrientNeeded, Long> {
-}
