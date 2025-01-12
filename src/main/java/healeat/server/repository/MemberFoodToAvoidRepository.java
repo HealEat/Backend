1820ef21b787e06c0661ca5a707b29f9251b4c05
minki <159312137+iKniMiohC@users.noreply.github.com>
2025-01-13 00:56:46 +0900

    [REMOVE] 파일 (MemberFoodToAvoidRepository)

--- src/main/java/healeat/server/repository/MemberFoodToAvoidRepository.java
+++
@@ -1,7 +0,0 @@
-package healeat.server.repository;
-
-import healeat.server.domain.mapping.MemberFoodToAvoid;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberFoodToAvoidRepository extends JpaRepository<MemberFoodToAvoid, Long> {
-}
