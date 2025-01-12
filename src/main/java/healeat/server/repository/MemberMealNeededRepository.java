580febb37380af94b5bf9150f3089562aa44dbc9
minki <159312137+iKniMiohC@users.noreply.github.com>
2025-01-13 00:57:40 +0900

    [REMOVE] 파일 (MemberMealNeededRepository)

--- src/main/java/healeat/server/repository/MemberMealNeededRepository.java
+++
@@ -1,7 +0,0 @@
-package healeat.server.repository;
-
-import healeat.server.domain.mapping.MemberMealNeeded;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberMealNeededRepository extends JpaRepository<MemberMealNeeded, Long> {
-}
