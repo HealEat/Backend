ce19d4968be862399b32b2fef9c201e7ddfa0239
Godot-B <bae0821@gmail.com>
2025-01-13 02:34:51 +0900

    #22 [CORRECT] 리포지토리 메서드 이름도 수정

--- src/main/java/healeat/server/repository/HealthInfoAnswerRepository.java
+++ src/main/java/healeat/server/repository/HealthInfoAnswerRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HealthInfoAnswerRepository extends JpaRepository<HealthInfoAnswer, Long> {
 
-    void deleteByMemberHealthInfoQuestion(MemberHealQuestion memberHealQuestion);
+    void deleteByMemberHealQuestion(MemberHealQuestion memberHealQuestion);
 }
