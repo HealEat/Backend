b7f57d22e109c68ac6f1998521fa636570ea0477
yoonseo8167 <yoonseo8167@sookmyung.ac.kr>
2025-01-15 00:06:33 +0900

    MemberHealthInfoService의 return 값에 converter 사용하도록 수정

--- src/main/java/healeat/server/service/MemberHealthInfoService.java
+++ src/main/java/healeat/server/service/MemberHealthInfoService.java
@@ -1,5 +1,6 @@
 package healeat.server.service;
 
+import healeat.server.converter.MemberHealthInfoConverter;
 import healeat.server.domain.HealthInfoAnswer;
 import healeat.server.domain.Member;
 import healeat.server.domain.MemberHealQuestion;
@@ -21,47 +22,36 @@ public class MemberHealthInfoService {
     private final MemberRepository memberRepository;
     private final MemberHealQuestionRepository questionRepository;
     private final HealthInfoAnswerRepository answerRepository;
+    private final MemberHealthInfoConverter converter;
 
+    // 회원의 건강 정보 질문 및 답변 데이터 전체 조회
     @Transactional(readOnly = true)
     public MemberHealthInfoResponseDto getMemberHealthInfo(Long memberId) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new IllegalArgumentException("Member not found"));
 
-        // 회원의 건강 정보 질문 및 답변 데이터 전체 조회
         List<QuestionResponseDto> questions = member.getMemberHealQuestions().stream()
-                .map(question -> QuestionResponseDto.builder()
-                        .questionId(question.getId())
-                        .questionText(question.getQuestion().name())
-                        .answers(question.getHealthInfoAnswers().stream()
-                                .map(answer -> answer.getAnswer().name())
-                                .toList())
-                        .build())
+                .map(converter::toQuestionResponseDto)
                 .toList();
 
-        return MemberHealthInfoResponseDto.builder()
-                .memberId(memberId)
-                .questions(questions)
-                .build();
+        return converter.toMemberHealthInfoResponseDto(member, questions);
     }
 
+    // 특정 질문 조회하기
     @Transactional(readOnly = true)
     public QuestionResponseDto getQuestion(Long questionId) {
         MemberHealQuestion question = questionRepository.findById(questionId)
                 .orElseThrow(() -> new IllegalArgumentException("Question not found"));
 
-        // 특정 질문 조회하기
-        return QuestionResponseDto.builder()
-                .questionId(question.getId())
-                .questionText(question.getQuestion().name())
-                .answers(question.getHealthInfoAnswers().stream()
-                        .map(answer -> answer.getAnswer().name())
-                        .toList())
-                .build();
+        return converter.toQuestionResponseDto(question);
     }
 
     // 특정 질문에 대한 회원의 답변 저장
     @Transactional
     public AnswerResponseDto saveAnswer(Long memberId, Long questionId, AnswerRequestDto request) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("Member not found"));
 
         MemberHealQuestion question = questionRepository.findById(questionId)
                 .orElseThrow(() -> new IllegalArgumentException("Question not found"));
@@ -79,13 +69,7 @@ public class MemberHealthInfoService {
 
         answerRepository.saveAll(answers);
 
-        return AnswerResponseDto.builder()
-                .questionId(questionId)
-                .memberId(memberId)
-                .selectedOptions(request.getSelectedAnswers().stream()
-                        .map(Enum::name)
-                        .toList())
-                .build();
+        return converter.toAnswerResponseDto(memberId, questionId, request.getSelectedAnswers());
     }
 
 }
