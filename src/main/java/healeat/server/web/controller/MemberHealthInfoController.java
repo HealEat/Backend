370e4e9cc9a599ac2170c030d32b6905422d91f3
yoonseo8167 <yoonseo8167@sookmyung.ac.kr>
2025-01-15 19:01:53 +0900

    @Operation 추가, 코드 오타 수정

--- src/main/java/healeat/server/web/controller/MemberHealthInfoController.java
+++ src/main/java/healeat/server/web/controller/MemberHealthInfoController.java
@@ -6,8 +6,8 @@ import healeat.server.web.dto.AnswerRequestDto;
 import healeat.server.web.dto.AnswerResponseDto;
 import healeat.server.web.dto.MemberHealthInfoResponseDto;
 import healeat.server.web.dto.QuestionResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,19 +17,19 @@ public class MemberHealthInfoController {
 
     private final MemberHealthInfoService memberHealthInfoService;
 
-    // 1. 사용자 앱 사용 목적과 질문 및 답변 전체 조회 api
+    @Operation(summary = "사용자 앱 사용 목적과 질문 및 답변 전체 조회 API")
     @GetMapping("/{memberId}/purposes")
     public ApiResponse<MemberHealthInfoResponseDto> getMemberHealthInfo(@PathVariable Long memberId) {
         return ApiResponse.onSuccess(memberHealthInfoService.getMemberHealthInfo(memberId));
     }
 
-    // 2. 특정 질문 조회하기 api
+    @Operation(summary = "특정 질문 조회하기 API")
     @GetMapping("/questions/{questionId}")
     public ApiResponse<QuestionResponseDto> getQuestion(@PathVariable Long questionId) {
         return ApiResponse.onSuccess(memberHealthInfoService.getQuestion(questionId));
     }
 
-    // 3. 특정 질문 답변 저장하기 api
+    @Operation(summary = "특정 질문 답변 저장하기 API")
     @PutMapping("/{memberId}/questions/{questionId}/answers")
     public ApiResponse<AnswerResponseDto> saveAnswer(
             @PathVariable Long memberId,
