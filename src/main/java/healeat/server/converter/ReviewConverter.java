8d68dbfc11843e5b10e4367f8f3ad20b11218d79
Godot-B <bae0821@gmail.com>
2025-01-17 00:40:11 +0900

    #35 [REFACTOR] toReviewPreviewDto도 간결하게 수정 완료

--- src/main/java/healeat/server/converter/ReviewConverter.java
+++ src/main/java/healeat/server/converter/ReviewConverter.java
@@ -4,30 +4,15 @@ import healeat.server.domain.mapping.Review;
 import healeat.server.web.dto.StoreResonseDto;
 import org.springframework.data.domain.Page;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class ReviewConverter {
 
     public static StoreResonseDto.ReviewPreviewDto toReviewPreviewDto(Review review) {
 
-        Map<Purpose, List<String>> currentPurposes = review.getCurrentPurposes();
-        List<String> purposeStringList = new ArrayList<>();
-        List<Purpose> orderedPurposes = Arrays.asList(Purpose.SICK, Purpose.VEGET, Purpose.DIET);
-        if (currentPurposes != null) {
-            orderedPurposes.forEach(purpose -> {
-                List<String> values = currentPurposes.get(purpose);
-                if (values != null) {
-                    purposeStringList.addAll(values);
-                }
-            });
-        }
-
         return StoreResonseDto.ReviewPreviewDto.builder()
                 .name(review.getMember().getName())
-                .currentPurposes(purposeStringList)
+                .currentPurposes(review.getCurrentPurposes())
                 .reviewId(review.getId())
                 .totalScore(review.getTotalScore())
                 .images(/*review.getReviewImageList()*/ null) // 이미지 CRUD 구현 필요
