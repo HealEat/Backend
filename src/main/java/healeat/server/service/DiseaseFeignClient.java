71d0d43dd0158f78e9cb3f04cd007f225740684d
yoonseo8167 <yoonseo8167@sookmyung.ac.kr>
2025-01-17 01:59:38 +0900

    #29 Disease api key 숨기기

--- src/main/java/healeat/server/service/DiseaseFeignClient.java
+++ src/main/java/healeat/server/service/DiseaseFeignClient.java
@@ -1,16 +1,17 @@
 package healeat.server.service;
 
+import healeat.server.config.DiseaseFeignConfig;
+import healeat.server.config.FeignConfig;
 import healeat.server.web.dto.DiseaseApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "disease-api-client", url = "http://apis.data.go.kr/B551182/diseaseInfoService1")
+@FeignClient(name = "disease-api-client", url = "http://apis.data.go.kr/B551182/diseaseInfoService1", configuration = DiseaseFeignConfig.class)
 public interface DiseaseFeignClient {
 
     @GetMapping("/getDissNameCodeList1")
     DiseaseApiResponse getDiseases(
-            @RequestParam("serviceKey") String serviceKey,
             @RequestParam("numOfRows") int numOfRows,   // 한 페이지 결과 수
             @RequestParam("pageNo") int pageNo,         // 페이지 번호
             @RequestParam("sickType") int sickType,     // 상병 구분 (1: 3단 상병, 2, 4단 상병)
