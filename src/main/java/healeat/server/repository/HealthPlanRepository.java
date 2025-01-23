494ab7c48c259cd0e752d374fb6c0d719bace1dc
iKniMiohC <fnwm7401@naver.com>
2025-01-23 19:29:37 +0900

    [FIX] healthPlan member 없을 때 테스트 데이터를 통한 API 사용 가능 구현

--- src/main/java/healeat/server/repository/HealthPlanRepository.java
+++ src/main/java/healeat/server/repository/HealthPlanRepository.java
@@ -1,6 +1,7 @@
 package healeat.server.repository;
 
 import healeat.server.domain.HealthPlan;
+import healeat.server.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
