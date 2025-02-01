67ad87dadf8952e227598674e320df41e71c50ba
Godot-B <bae0821@gmail.com>
2025-02-02 00:25:15 +0900

    #127 [FEAT] Caffeine 설정 및 적용, SearchResult, 서비스 로직 간소화

---
+++ src/main/java/healeat/server/config/CacheConfig.java
@@ -0,0 +1,26 @@
+package healeat.server.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+
+@Configuration
+@EnableCaching  // 캐싱 기능 활성화
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("searchResult");
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .maximumSize(10000)  // 최대 10,000개 저장
+                .expireAfterWrite(60, TimeUnit.MINUTES)  // 60분 후 만료
+                .recordStats());  // 캐시 통계 활성화
+        return cacheManager;
+    }
+}
