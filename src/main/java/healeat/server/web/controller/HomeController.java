1daf21005af61c106e8611dcae1167afce1e9c0e
Godot-B <bae0821@gmail.com>
2025-02-06 17:45:33 +0900

    #142 [FEAT] 스웨거 description 추가

--- src/main/java/healeat/server/web/controller/HomeController.java
+++ src/main/java/healeat/server/web/controller/HomeController.java
@@ -19,10 +19,13 @@ public class HomeController {
     private final MemberRepository memberRepository;
     private final StoreQueryServiceImpl storeQueryServiceImpl;
 
+    @Operation(summary = "홈 화면에서 추천 가게 리스트를 조회합니다.", description =
+            """ 
+                    Request Body에
+                    사용자 x, y, 조사할 반경(radius)를 받아서 추천 가게 목록을 조회합니다.
+                    
+                    위치 또는 반경의 200m 내 오차까지 동일한 캐시에서 반환됩니다.""")
     @PostMapping
-    @Operation(summary = "홈 화면에서 추천 가게 리스트를 조회합니다.",
-            description = "Request Body에 사용자 x, y, 조사할 반경(radius)를 받아서 추천 가게 목록을 조회합니다." +
-                    "")
     public ApiResponse<StoreResonseDto.StorePreviewDtoList> getHomeList(
             @AuthenticationPrincipal Member member,
             @RequestParam Integer page,
