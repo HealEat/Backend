2f5bcaf6b34c3bab7a81944edd742a84db73d3cc
Godot-B <bae0821@gmail.com>
2025-02-06 17:18:34 +0900

    #142 [FEAT] 홈 가게 추천 구현

--- src/main/java/healeat/server/web/controller/HomeController.java
+++ src/main/java/healeat/server/web/controller/HomeController.java
@@ -3,6 +3,7 @@ package healeat.server.web.controller;
 import healeat.server.apiPayload.ApiResponse;
 import healeat.server.domain.Member;
 import healeat.server.repository.MemberRepository;
+import healeat.server.service.StoreQueryServiceImpl;
 import healeat.server.web.dto.StoreRequestDto;
 import healeat.server.web.dto.StoreResonseDto;
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,27 +17,19 @@ import org.springframework.web.bind.annotation.*;
 public class HomeController {
 
     private final MemberRepository memberRepository;
+    private final StoreQueryServiceImpl storeQueryServiceImpl;
 
-    @GetMapping
+    @PostMapping
     @Operation(summary = "홈 화면에서 추천 가게 리스트를 조회합니다.",
-            description = "홈 화면에서 현재 위치를 기반으로 추천 가게 리스트를 조회합니다.")
+            description = "Request Body에 사용자 x, y, 조사할 반경(radius)를 받아서 추천 가게 목록을 조회합니다." +
+                    "")
     public ApiResponse<StoreResonseDto.StorePreviewDtoList> getHomeList(
             @AuthenticationPrincipal Member member,
-            @ModelAttribute StoreRequestDto.HealEatRequestDto request){
+            @RequestParam Integer page,
+            @RequestBody StoreRequestDto.HealEatRequestDto request){
 
         Member testMember = memberRepository.findById(999L).get();
 
-        return ApiResponse.onSuccess(null);
-    }
-
-    @GetMapping("/{storeId}")
-    @Operation(summary = "홈 화면에서 추천 가게 미리보기를 조회합니다.",
-            description = "홈 화면에서 현재 위치를 기반으로 추천된 가게 리스트 중 하나를 선택했을 때 보이는 미리보기 화면입니다.")
-    public ApiResponse<StoreResonseDto.HomeStorePreviewDto> getStorePreview(
-            @PathVariable Long storeId, @AuthenticationPrincipal Member member){
-
-        Member testMember = memberRepository.findById(999L).get();
-
-        return ApiResponse.onSuccess(null);
+        return ApiResponse.onSuccess(storeQueryServiceImpl.recommendAndMapStores(testMember, page, request));
     }
 }
