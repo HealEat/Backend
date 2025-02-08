c2e22b4374b94bf43616784f6d85dbf7f7eeadd2
iKniMiohC <fnwm7401@naver.com>
2025-02-08 21:22:04 +0900

    #158 [MODIFY] 멤버 탈퇴시 관련 데이터 삭제

--- src/main/java/healeat/server/service/UnlinkService.java
+++ src/main/java/healeat/server/service/UnlinkService.java
@@ -1,18 +1,42 @@
 package healeat.server.service;
 
+import healeat.server.domain.HealthPlan;
+import healeat.server.domain.Member;
+import healeat.server.repository.HealthPlanRepository;
 import healeat.server.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UnlinkService {
 
     private final MemberRepository memberRepository;
+    private final HealthPlanRepository healthPlanRepository;
+    private final HealthPlanService healthPlanService;
+    private final ImageService imageService;
 
     @Transactional
     public void deleteKakaoMember(String provider, String providerId) {
+
+        /*********************** 멤버 관련 데이터 삭제 (건강관리목표 / 프로필) ***********************/
+        // 건강관리목표 삭제
+        Optional<Member> member = memberRepository.findByProviderAndProviderId(provider, providerId);
+
+        List<HealthPlan> healthPlans = healthPlanRepository.findByMember(member.get());
+        if (!healthPlans.isEmpty()) {
+            for(HealthPlan healthPlan : healthPlans) {
+                healthPlanService.deleteHealthPlan(healthPlan.getId());
+            }
+        }
+
+        // 프로필 삭제
+        imageService.deleteProfileImage(member.get().getId());
+
         System.out.println(" DB에서 삭제: provider=" + provider + ", providerId=" + providerId);
         memberRepository.deleteByProviderAndProviderId(provider, providerId);
     }
