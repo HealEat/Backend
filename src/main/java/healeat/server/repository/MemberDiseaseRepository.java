369bd472ac833a0d850e1f4e7a652583fa00381d
Godot-B <bae0821@gmail.com>
2025-02-14 23:47:33 +0900

    #222 [MERGE] 머지 해결

--- src/main/java/healeat/server/repository/MemberDiseaseRepository.java
+++ src/main/java/healeat/server/repository/MemberDiseaseRepository.java
@@ -1,5 +1,6 @@
 package healeat.server.repository;
 
+import healeat.server.domain.Disease;
 import healeat.server.domain.Member;
 import healeat.server.domain.mapping.MemberDisease;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,8 @@ import java.util.List;
 
 public interface MemberDiseaseRepository extends JpaRepository<MemberDisease, Long> {
     List<MemberDisease> deleteAllByMember(Member member);
+
+    Boolean existsByMemberAndDisease(Member member, Disease disease);
 
     List<MemberDisease> findByMember(Member member);
 }
